76efa6860bb52b751ea425fbacecbbdc5eb164dd
Hersch Nathan <hnathan628@gmail.com>
2024-11-18 13:05:58 -0500

    created bme280 sensor files, using the defs provided by API

---
+++ SSLFirmware/main/Sensors/bme280/bme280_i2c.c
@@ -0,0 +1,5 @@
+//bme280_i2c.c
+//use the bme280 sensor over i2c using the i2c_wrapper
+//Developed for RockSat-X GHOST
+//Based off of UncleRus ESP32 bmp280 library and bme280_sensor api
+//Developed by Hersch Nathan
