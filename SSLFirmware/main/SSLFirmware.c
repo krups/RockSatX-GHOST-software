777d3cda42eb76354a3a5b0d82bfe028ea898b83
Hersch Nathan <hnathan628@gmail.com>
2024-11-19 13:21:37 -0500

    try try again...getting there

--- SSLFirmware/main/SSLFirmware.c
+++ SSLFirmware/main/SSLFirmware.c
@@ -4,15 +4,18 @@
 //Based on Matt Ruffner's KREPE 2 firmware
 //Developed by Hersch Nathan, Alex Barrera
 
-#include <config.h> 
-#include "freertos/task.h" //backbone of this firmware
-#include "freertos/FreeRTOS.h" //backbone of this firmware
-#include <freertos/timers.h>
-#include <freertos/queue.h>
-#include <freertos/semphr.h>
+
+#include "config.h" 
+
+#include "freertos/FreeRTOS.h" 
+#include "freertos/task.h" 
+#include "freertos/timers.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+
 #include <driver/i2c.h>
 #include <stdio.h>
-#include "BME280_sensor.h"
+#include "BME280_sensor/BME280_sensor.h"
 #include "i2c_manager.h"
 #include "Fake_Sensor.h"
 
