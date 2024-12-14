833172fd9dfde99a0d7707800141967bcd270ad7
cshcmidt <7ross.schmidt@gmail.com>
2024-12-14 17:06:50 -0500

    changed to camel case

--- SSLFirmware/main/BucketManager.c
+++ SSLFirmware/main/BucketManager.c
@@ -7,51 +7,51 @@
 // one of the main issues is the the bucket itself is not allocated on the heap yet
 struct bucket{
     topic* tpc;// pointer to the topic that this bucket is in
-    bool in_use;// the bucket is not in use by default
+    bool inUse;// the bucket is not in use by default
     void* buf;
     int size;
     int capacity;
-    int data_size;
+    int dataSize;
 };
 struct topic{
-    int num_buckets;
+    int numBuckets;
     SemaphoreHandle_t count;// this is the counting semaphore that determines if there are any available buckets
-    bucket* bk_list;
+    bucket* bkList;
 };
 
 // makes a group and fills it with the buckets of the sizes determined by bucket_sizes
 // must be called like _declare_group<type>(args) where the type determines the type that the buckets are set up to store 
 
-bucket* declare_topic( int bucket_sizes,int num_buckets,int data_size){
-    topic* tpc_ptr =(topic *)malloc(sizeof(topic*)); 
-    bucket lst[num_buckets];
-    tpc_ptr->bk_list = &lst;
+bucket* declare_topic( int bucket_sizes,int numBuckets,int dataSize){
+    topic* tpcPtr =(topic *)malloc(sizeof(topic*)); 
+    bucket lst[numBuckets];
+    tpcPtr->bkList = &lst;
     // fill the topic with its buckets
-    for(int i=0;i<num_buckets;i++){
+    for(int i=0;i<numBuckets;i++){
         //stack allocated bucket struct that contains heap allocated buffer
         bucket b;
-        void *buf_ptr = (void *)malloc(bucket_sizes*data_size);
+        void *buf_ptr = (void *)malloc(bucket_sizes*dataSize);
         b.buf = buf_ptr;
-        b.tpc = tpc_ptr;
-        b.data_size = data_size;
+        b.tpc = tpcPtr;
+        b.dataSize = dataSize;
         //puts the queue handle into a map that will map it to a pointer to the bucket that queue handle belongs to
         
-        tpc_ptr->bk_list[i]=b;
+        tpcPtr->bkList[i]=b;
     } 
     
-    tpc_ptr->count = xSemaphoreCreateCounting(bucket_sizes,bucket_sizes);
-    tpc_ptr->num_buckets = num_buckets;
-    return tpc_ptr;
+    tpcPtr->count = xSemaphoreCreateCounting(bucket_sizes,bucket_sizes);
+    tpcPtr->numBuckets = numBuckets;
+    return tpcPtr;
 }
 // finds the first available bucket to read from and returns a pointer to it to the calling task
 // this would also give a very similar effect to subscribing to a topic in a pub sub model
-bucket* get_read_bucket(topic *tpc_ptr){
-    if(xSemaphoreTake(tpc_ptr->count, (TickType_t) 10)==pdTRUE){    
-        for(int i=0; i<tpc_ptr->num_buckets;i++){
-            if(!(tpc_ptr->bk_list[i].in_use)){
+bucket* get_read_bucket(topic *tpcPtr){
+    if(xSemaphoreTake(tpcPtr->count, (TickType_t) 10)==pdTRUE){    
+        for(int i=0; i<tpcPtr->numBuckets;i++){
+            if(!(tpcPtr->bkList[i].inUse)){
                 
-                tpc_ptr->bk_list[i].in_use = true;
-                return &(tpc_ptr->bk_list[i]);
+                tpcPtr->bkList[i].inUse = true;
+                return &(tpcPtr->bkList[i]);
             }
         }
     }
@@ -59,30 +59,30 @@ bucket* get_read_bucket(topic *tpc_ptr){
     return NULL;
 }
 
-bucket* get_write_bucket(topic* tpc_ptr){
-    if(xSemaphoreTake(tpc_ptr->count,(TickType_t) 10)==pdTRUE){    
-        for(int i=0; i<tpc_ptr->num_buckets;i++){
-            if(!tpc_ptr->bk_list[i].in_use){
-                tpc_ptr->bk_list[i].in_use = true;
-                return &(tpc_ptr->bk_list[i]);
+bucket* getWriteBucket(topic* tpcPtr){
+    if(xSemaphoreTake(tpcPtr->count,(TickType_t) 10)==pdTRUE){    
+        for(int i=0; i<tpcPtr->numBuckets;i++){
+            if(!tpcPtr->bkList[i].inUse){
+                tpcPtr->bkList[i].inUse = true;
+                return &(tpcPtr->bkList[i]);
             }
         }
     }
     return NULL;
 }
 
-void give_bucket(bucket* bk_ptr){
-    xSemaphoreGive(bk_ptr->tpc->count);
-    bk_ptr->in_use=false;
+void giveBucket(bucket* bkPtr){
+    xSemaphoreGive(bkPtr->tpc->count);
+    bkPtr->inUse=false;
 }
 // reads num elements from the bucket pointed to by b_ptr
 // ex: bMessageRecieve(receiver,bucket,num=4) on a bucket storing ints would read 4 ints from the bucket into the reciever pointer
 // note the reciever pointer must be a void pointer that is then cast to the intended type and dereferenced 
-void b_message_recieve(void* r_ptr,bucket* b_ptr,int num){
+void bMessageRecieve(void* rPtr,bucket* bPtr,int num){
     //checks if the number of bytes in the bucket is enough for what is being asked for
     // then copies into the reciever pointer starting at the end of the bucket array - bytes
-    if(b_ptr->size >= num){
-        memcpy(r_ptr,b_ptr+b_ptr->size*b_ptr->data_size-num*b_ptr->data_size,num*b_ptr->data_size);
+    if(bPtr->size >= num){
+        memcpy(rPtr,bPtr+bPtr->size*bPtr->dataSize-num*bPtr->dataSize,num*bPtr->dataSize);
     }else{
         // generic debug message such as bucket does not have enough data to read num bytes
     }
@@ -90,9 +90,9 @@ void b_message_recieve(void* r_ptr,bucket* b_ptr,int num){
 }
 // writes num typed data points to the bucket pointed to by b_ptr
 // ex: bMessageWrite(sender,bucket,num=4) on a bucket storing ints would copy 4 ints from the sender pointer into the bucket
-void bMessageWrite(void* s_ptr,bucket* b_ptr,int num){
-    if(b_ptr->capacity-b_ptr->size>=num){
-        memcpy(b_ptr+b_ptr->size*b_ptr->data_size,s_ptr,num*b_ptr->data_size);
+void bMessageWrite(void* sPtr,bucket* bPtr,int num){
+    if(bPtr->capacity-bPtr->size>=num){
+        memcpy(bPtr+bPtr->size*bPtr->dataSize,sPtr,num*bPtr->dataSize);
     }else{
         //generic error message warning about a full bucket
     }
